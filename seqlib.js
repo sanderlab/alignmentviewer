17391d59a240c6c0686c65552f23f8dfc347e88b
Roc Reguant <roc.reguant@gmail..com>
2018-02-13 14:53:39 +0100

    saving a non working progress

--- seqlib.js
+++ seqlib.js
@@ -992,6 +992,118 @@
 		this.update();	*/
 	}};
 	}
+
+	// ---------- Doing graph -------------------------------------------------------
+
+function initGraph(msa){
+	var svg = d3.select("#MSApairwiseGraph2"),
+    width = +svg.attr("width"),
+    height = +svg.attr("height");
+
+	//var color = d3.scaleOrdinal(d3.schemeCategory20);
+
+	var simulation = d3.forceSimulation()
+	    .force("link", d3.forceLink().id(function(d) { return d.id; }))
+	    .force("charge", d3.forceManyBody())
+	    .force("center", d3.forceCenter(width / 2, height / 2));
+
+
+	var graph0 = new Object();
+
+	graph0.nodes = [];
+	graph0.links = [];
+
+	var i = 0;
+
+	for (var elm in msa.pwhash){
+		if (i < msa.h) {
+			var singleObj = {};
+		    singleObj['id'] = i;
+			graph0.nodes.push(singleObj);
+		}
+		i = i + 1
+		sp = key.split(',');
+		var val = msa.pwhash[key];
+
+		if (val > 0.3){
+ 			var parentNode = {};
+			var sNode = {};
+			var tNode = {};
+			sNode['id'] = sp[0];
+			tNode['id'] = sp[1];
+		    parentNode['source'] = sNode;
+		    parentNode['target'] = tNode;
+		    parentNode['value'] = 1 - val;
+			graph0.links.push(parentNode);
+		}
+	}
+
+	var graph = JSON.stringify(graph0);
+
+	function generateGraph(graph) {
+
+	  var link = svg.append("g")
+	      .attr("class", "links")
+	    .selectAll("line")
+	    .data(graph.links)
+	    .enter().append("line")
+	      .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
+
+	  var node = svg.append("g")
+	      .attr("class", "nodes")
+	    .selectAll("circle")
+	    .data(graph.nodes)
+	    .enter().append("circle")
+	      .attr("r", 5)
+	      .attr("fill", function(d) { return "rgb(0, 69, 41)"; })
+	      .call(d3.drag()
+	          .on("start", dragstarted)
+	          .on("drag", dragged)
+	          .on("end", dragended));
+
+	  node.append("title")
+	      .text(function(d) { return d.id; });
+
+	  simulation
+	      .nodes(graph.nodes)
+	      .on("tick", ticked);
+
+	  simulation.force("link")
+	      .links(graph.links);
+
+	  function ticked() {
+	    link
+	        .attr("x1", function(d) { return d.source.x; })
+	        .attr("y1", function(d) { return d.source.y; })
+	        .attr("x2", function(d) { return d.target.x; })
+	        .attr("y2", function(d) { return d.target.y; });
+
+	    node
+	        .attr("cx", function(d) { return d.x; })
+	        .attr("cy", function(d) { return d.y; });
+	  }
+	};
+
+	generateGraph(graph);
+
+	function dragstarted(d) {
+	  if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+	  d.fx = d.x;
+	  d.fy = d.y;
+	}
+
+	function dragged(d) {
+	  d.fx = d3.event.x;
+	  d.fy = d3.event.y;
+	}
+
+	function dragended(d) {
+	  if (!d3.event.active) simulation.alphaTarget(0);
+	  d.fx = null;
+	  d.fy = null;
+	}
+}
+
 	// ---------- kMins clustering ----- work in progress -------------------------------------------------------
 	function testkMinsClustering() {
 		var Nclu = 4;
